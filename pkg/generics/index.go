757f0f3404a123caef66771b8ff1e7e517215b81
Andrey Radkevich <andrey@48pacific.com>
2022-07-17 14:30:40 +0300

    generics

---
+++ pkg/generics/index.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func CreateSlice[T int | string](length int) []T {
+	return make([]T, length)
+}
+
+func main() {
+	intSlice := CreateSlice[int](10)
+	stringSlice := CreateSlice[string](10)
+
+	fmt.Println(intSlice)    // [0,0,0,0,0,0,0,0,0,0]
+	fmt.Println(stringSlice) // [         ]
+}
